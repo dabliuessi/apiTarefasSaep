7f856a6f39d17b70833555055492989d12ff0542
dabliuessi <wsmdm555@gmail.com>
2025-06-17 20:24:48 -0300

    commit01

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -3,6 +3,15 @@ const Usuario = require('../models/usuario');
 const router = express.Router();
 
 // Criar novo usuário
+router.get('/', async (req, res) => {
+  try {
+    const usuarios = await Usuario.findAll();
+    res.status(200).json(usuarios);
+  } catch (error) {
+    console.error('Erro ao buscar usuários:', error);
+    res.status(500).json({ error: 'Erro interno do servidor ao buscar usuários.' });
+  }
+});
 router.post('/', async (req, res) => {
   try {
     const { nome, email } = req.body;
